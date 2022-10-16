3bbe48766488de590ed6fe2ea07d06afa4f79407
Jakub.Mamala <G00408184@atu.ie>
2022-10-16 21:22:21 +0100

    Added new file called Rectangle

---
+++ src/main/java/ie/atu/week5/Rectangle.java
@@ -0,0 +1,22 @@
+package ie.atu.week5;
+
+public class Rectangle {
+    private double lenght;
+    private double width;
+
+    public double getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(double lenght) {
+        this.lenght = lenght;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
