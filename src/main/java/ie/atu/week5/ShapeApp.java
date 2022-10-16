efcd3e2ce1e296c62719f430473481c21273a9e2
Jakub.Mamala <G00408184@atu.ie>
2022-10-16 21:07:49 +0100

    Prompt user for length

--- src/main/java/ie/atu/week5/ShapeApp.java
+++ src/main/java/ie/atu/week5/ShapeApp.java
@@ -1,5 +1,14 @@
 package ie.atu.week5;
 
+import java.util.Scanner;
+
 public class ShapeApp {
+
+    public static void main(String[] args) {
+        System.out.println("please enter the width of the rectangle");
+        Scanner input = new Scanner(System.in);
+        double lenght = input.nextDouble();
+        System.out.println("You entered : " + lenght);
+    }
 
 }
